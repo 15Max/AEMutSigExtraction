16b1b6071d69fa3978c4abb90c8c7d8bcf9c8231
Marta Lucas <94189892+15Max@users.noreply.github.com>
2024-12-05 16:15:01 +0100

    Implemented just the pg version

---
+++ models/NMF.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn.functional as F
+
+class AE_NMF(torch.nn.Module):
+    """
+    Autoencoder for Non-negative Matrix Factorization using Projected Gradient.
+    """
+    def __init__(self, input_dim, latent_dim):
+        super().__init__()
+        self.enc_weight = torch.nn.Parameter(torch.rand(input_dim, latent_dim))
+        self.dec_weight = torch.nn.Parameter(torch.rand(latent_dim, input_dim))
+
+    def forward(self, x):
+        x = torch.matmul(x, F.relu(self.enc_weight))
+        x = torch.matmul(x, F.relu(self.dec_weight))
+        return x
+    
+    
+def train(model, training_data, criterion, optimizer, tol = 1e-3, relative_tol = True, max_iter = 100000):
+    training_data_tensor = torch.tensor(training_data.values, dtype = torch.float32)
+
+    training_loss = [1e10]
+    relative_diff = float('inf')
+
+    iter = 0
+    while relative_diff > tol and iter < max_iter:
+        optimizer.zero_grad()
+        output = model(training_data_tensor)
+        loss = criterion(output, training_data_tensor)
+        loss.backward()
+        optimizer.step()
+
+        training_loss.append(loss.item())
+        
+        if relative_tol:
+            denominator = training_loss[-2]
+        else:
+            denominator = 1 
+        relative_diff = abs(training_loss[-1] - training_loss[-2])/denominator
+        
+        # Go to next iteration
+        iter += 1
+
+        with torch.no_grad():
+            for param in model.parameters():
+                param.clamp_(min=0)
+
+        # Constraints on final signature and exposure matrices
+        enc_mat = (model.enc_weight.data).numpy().clip(min=0)
+        sig_mat = (training_data @ enc_mat).to_numpy()
+        exp_mat = (model.dec_weight.data).numpy().clip(min=0)
+
+    return (model, training_loss, sig_mat, exp_mat, enc_mat, iter)
