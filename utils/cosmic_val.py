b1387a50f57f1bd1dfb1789e260165fde7ff8b61
15Max <94189892+15Max@users.noreply.github.com>
2025-01-24 11:04:47 +0100

    added skeleton

--- utils/cosmic_val.py
+++ utils/cosmic_val.py
@@ -1,1 +1,9 @@
 import numpy as np 
+import pandas as pd
+
+
+from scipy.optimize import linear_sum_assignment
+from sklearn.metrics.pairwise import cosine_similarity
+
+def compute_match():
+    pass
