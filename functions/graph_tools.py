7edf721c8bd76c067b7a80c4a88670430b39c9ad
15Max <94189892+15Max@users.noreply.github.com>
2025-03-15 18:10:21 +0100

    fixed colors

--- functions/graph_tools.py
+++ functions/graph_tools.py
@@ -127,15 +127,14 @@ def plot_cosine_similarity_matrix(all_matches: pd.DataFrame, title : str ="Cosin
         'SBS10c' : custom_cmap(3),
         'SBS12' : custom_cmap(13),
         'SBS13' : custom_cmap(0),
-        'SBS15' : custom_cmap(15),
+        'SBS15' : custom_cmap(2),
         'SBS26' : custom_cmap(4),
         'SBS40a': custom_cmap(18),
         'SBS40c' : custom_cmap(19),
         'SBS44' : custom_cmap(7),
-        'SBS52' : custom_cmap(14),
+        'SBS52' : custom_cmap(8),
         'SBS57' : custom_cmap(10),
         }
-
     
    # Create a color matrix
     N, M = all_signatures.shape
@@ -198,25 +197,45 @@ def plot_cosine_similarity_matrix(all_matches: pd.DataFrame, title : str ="Cosin
 
 
 
-def plot_signature_frequency(all_matches: pd.DataFrame, title: str = "Signature Appearances", figsize: tuple = (12, 7), color: str = 'steelblue') -> None:
+    
+def plot_signature_frequency(all_matches: pd.DataFrame, title: str = "Signature Appearances", figsize: tuple = (12, 7), color: str = 'tab20') -> None:
     all_signatures = all_matches.iloc[:, 0::2]
 
     # Count occurrences of each signature across the entire dataset
     signature_counts = all_signatures.stack().value_counts().sort_values(ascending=False)
 
-    fig, ax = plt.subplots(figsize=figsize)
-    ax.bar(signature_counts.index, signature_counts.values, color=color, edgecolor='black', alpha=0.7)
+    custom_cmap = plt.cm.get_cmap(color, 20)
+    sig_to_color = {
+        'SBS1' : custom_cmap(16),
+        'SBS3' : custom_cmap(12),
+        'SBS5' : custom_cmap(11),
+        'SBS6' :custom_cmap(9),
+        'SBS8' : custom_cmap(1),
+        'SBS10a' : custom_cmap(5),
+        'SBS10c' : custom_cmap(3),
+        'SBS12' : custom_cmap(13),
+        'SBS13' : custom_cmap(0),
+        'SBS15' : custom_cmap(2),
+        'SBS26' : custom_cmap(4),
+        'SBS40a': custom_cmap(18),
+        'SBS40c' : custom_cmap(19),
+        'SBS44' : custom_cmap(7),
+        'SBS52' : custom_cmap(8),
+        'SBS57' : custom_cmap(10),
+        }
+    # Assign colors based on sig_to_color, fallback to default color if not in dict
+    bar_colors = [sig_to_color.get(sig, custom_cmap(2)) for sig in signature_counts.index]
 
-    
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.bar(signature_counts.index, signature_counts.values, color=bar_colors, edgecolor='black', alpha=0.7)
+
     ax.set_title(title, fontsize=20)
     ax.set_xlabel("Signatures", fontsize=16)
     ax.set_ylabel("Number of Occurrences", fontsize=16)
 
-   
     ax.set_xticks(range(len(signature_counts.index)))
     ax.set_xticklabels(signature_counts.index, rotation=0, ha='center', fontsize=12)
 
-   
     ax.yaxis.set_major_locator(mticker.MaxNLocator(integer=True))
     ax.tick_params(axis='y', labelsize=12)
 
